0ea67eba457d4d1c06f579dbad99417d86dfb638
Sendy <142478036+sendycarneiro@users.noreply.github.com>
2023-10-05 18:17:50 -0300

    Add document

---
+++ desafio.heroi.js
@@ -0,0 +1,20 @@
+let nomeHeroi = "Dovakin";
+let expHeroi = 10001;
+
+      if (expHeroi <= 1000){
+      console.log ("O Herói de nome: " +nomeHeroi +"está no nível de Ferro")
+      } else if (expHeroi <= 1001 && expHeroi<=2000){
+      console.log ("O Herói de nome: " +nomeHeroi +"está no nível de Bronze")
+      } else if (expHeroi <= 2001 && expHeroi <= 5000){
+      console.log ("O Herói de nome: " +nomeHeroi +"está no nível de Prata")
+      } else if (expHeroi <= 6001 && expHeroi <= 7000){
+      console.log ("O Herói de nome: " +nomeHeroi +"está no nível de Ouro")
+      } else if (expHeroi <= 7001 && expHeroi <= 8000){
+      console.log("O Herói de nome: " +nomeHeroi + "está no nível Platina")
+      } else if (expHeroi <= 8001 && expHeroi <= 9000){
+      console.log("O Herói de nome: " +nomeHeroi + "está no nível Ascendente")
+      } else if (expHeroi <= 9001 && expHeroi <= 10000){
+      console.log("O Herói de nome: " +nomeHeroi + "está no nível Imortal")
+      } else if (expHeroi <= 10001){
+      console.log("O Herói de nome: " +nomeHeroi + "está no nível Radiante")
+      }
